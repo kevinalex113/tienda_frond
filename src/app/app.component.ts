53db8a955b784240aa0a21285a8d90ad373311d8
kevinalex113 <kevinalex113@hotmail.com>
2024-05-18 14:18:18 -0600

    2.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 /*import { NgForm } from '@angular/forms';*/
 import { LoginComponent } from "./login/login.component";
 import { PruebasComponent } from "./pruebas/pruebas.component";
+import { PaginaPrincipalComponent } from "./pagina-principal/pagina-principal.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, LoginComponent, PruebasComponent]
+    imports: [RouterOutlet, LoginComponent, PruebasComponent, PaginaPrincipalComponent]
 })
 export class AppComponent { 
   title = 'tienda_frond';
